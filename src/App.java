dfe9020f52f9a6f0366ba4ad9199852b0a7fb6be
Felipe Tomasello <felipe.tomasello@gmail.com>
2022-07-25 11:24:43 -0300

    Refaorando classes e melhorando o projeto

--- src/App.java
+++ src/App.java
@@ -1,37 +1,33 @@
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         //fazer uma conexão http e buscar os top 250 filmes.
-        String url = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/NASA-APOD.json";
+        ExtratorConteudo extrator = new ExtratorConteudoNasa();
         
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
 
-        //extrair somente os dados que interesam (titulo, imagem, classificação).
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(json);
-        System.out.println(listaDeFilmes.size());
-
         //exibir e manipular os dados.
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+        
         var geradora = new GeradoraDeFigurinhas();
 
-        for (Map<String,String> filme : listaDeFilmes) {
+        for (int i = 0; i < 3; i++) {
 
-            String urlImage = filme.get("image");
-            String title = filme.get("title");
+            Conteudo conteudo = conteudos.get(i);
 
-            InputStream inputStream = new URL(urlImage).openStream();
-            var nomeArquivo = "saida/" + title + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
 
-            System.out.println(title);
+            System.out.println(conteudo.getTitulo());
             System.out.println();
         }
     }
--- src/ExtratorConteudoNasa.java
+++ src/ExtratorConteudoNasa.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class ExtratorConteudoNasa {
+public class ExtratorConteudoNasa implements ExtratorConteudo {
     
-    public List<Conteudo> extraConteudos (String json)
+    public List<Conteudo> extraiConteudos (String json) {
+         //extrair somente os dados que interesam (titulo, imagem, classificação).
+         var parser = new JsonParser();
+         List<Map<String, String>> listaDeatributos = parser.parse(json);
+         List<Conteudo> conteudos = new ArrayList<>();
+         //popular a lista de conteudos
+         for (Map<String, String> atributos: listaDeatributos) {
+            String titulo = atributos.get("title");
+            String urlImagem = atributos.get("url");
+
+            var conteudo = new Conteudo(titulo, urlImagem);
+            conteudos.add(conteudo);
+         }
+         return conteudos;
+    }
 }
