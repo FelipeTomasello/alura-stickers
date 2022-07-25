08de03f09eac2bf0e4e625185cee3f0f58e710e5
Felipe Tomasello <felipe.tomasello@gmail.com>
2022-07-25 11:32:28 -0300

    Correçoes classes extratoras

--- src/ExtratorConteudo.java
+++ src/ExtratorConteudo.java
@@ -2,6 +2,6 @@ import java.util.List;
 
 public interface ExtratorConteudo {
 
-    List<Conteudo> extraConteudos (String json);
-    
+    List<Conteudo> extraiConteudos (String json);
+
 }
--- src/ExtratorConteudoIMDB.java
+++ src/ExtratorConteudoIMDB.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class ExtratorConteudoIMDB implements ExtratorConteudo{
 
-    public List<Conteudo> extraConteudos (String json) {
+    public List<Conteudo> extraiConteudos (String json) {
 
         var parser = new JsonParser();
         List<Map<String,String>> listaDeAtributos = parser.parse(json);
